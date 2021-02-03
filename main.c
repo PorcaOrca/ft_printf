605ff5b2635f66d3edd6ed92d2592c42b36b7194
Lodovico Spazzini <lspazzin@e3r5p1.42roma.it>
2021-02-03 14:55:30 +0100

    a casa guajo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lspazzin <lspazzin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/30 11:01:45 by lspazzin          #+#    #+#             */
-/*   Updated: 2021/01/31 16:43:29 by lspazzin         ###   ########.fr       */
+/*   Updated: 2021/01/31 16:49:47 by lspazzin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int		main(int argc, char *argv[])
 
 	ez_gg = ft_calloc(sizeof(char), 2);
 	if (argc != 3)
-	dumb_ret = printf("Sucker printf = |%p|\n", ez_gg);
-	my_ret = ft_printf("My lol printf = |%p|\n", ez_gg);
+	dumb_ret = printf("Sucker printf = |%-023d|\n", -2345678);
+	my_ret = ft_printf("My lol printf = |%-023d|\n", -2345678);
 	printf("sucker return = |%d|\nmy lol return = |%d|", dumb_ret, my_ret);
 	return (0);
 }
